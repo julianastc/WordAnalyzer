1b775117b4bd64d2a73ccddadb5a004413526a02
= <=>
2025-01-21 15:07:02 -0300

    tests

--- WordAnalyzerUnitTest/WordAnalyzerUnitTest.cs
+++ WordAnalyzerUnitTest/WordAnalyzerUnitTest.cs
@@ -4,7 +4,10 @@ namespace WordAnalyzerUnitTest;
 public class AnalyzerUnitTest
 {
     [Theory]
+
     [InlineData("uma frase qualquer", "qualquer")]
+    [InlineData( "O sol brilhava intensamente enquanto as crianças brincavam no parque", "intensamente")]
+    [InlineData( "Um gato preto misterioso atravessou silenciosamente a rua deserta","silenciosamente")]
     public void LongestWord_ReceiveString_ReturnLongestWord(string words, string longWord)
     {
         // act
@@ -17,6 +20,10 @@ public class AnalyzerUnitTest
 
     [Theory]
     [InlineData("uma frase qualquer", "uma")]
+    [InlineData( "O sol brilhava intensamente enquanto as crianças brincavam no parque", "O")]
+    [InlineData( "Um gato preto misterioso atravessou silenciosamente a rua deserta","a")]
+
+
     public void ShortestWord_ReceiveString_ReturnShortestWord(string words, string shortWord)
     {
         // act
@@ -29,6 +36,10 @@ public class AnalyzerUnitTest
 
     [Theory]
     [InlineData("uma frase qualquer", "uma")]
+    [InlineData( "O sol brilhava intensamente enquanto as crianças brincavam no parque", "O")]
+    [InlineData( "Um gato preto misterioso atravessou silenciosamente a rua deserta","Um")]
+
+
     public void FirstWord_ReceiveString_ReturnFirstWord(string words, string first)
     {
         // act
@@ -41,6 +52,8 @@ public class AnalyzerUnitTest
 
     [Theory]
     [InlineData("uma frase qualquer", "qualquer")]
+    [InlineData( "O sol brilhava intensamente enquanto as crianças brincavam no parque", "parque")]
+    [InlineData( "Um gato preto misterioso atravessou silenciosamente a rua deserta","deserta")]
     public void LastWord_ReceiveString_ReturnLastWord(string words, string last)
     {
         // act
@@ -53,6 +66,8 @@ public class AnalyzerUnitTest
 
     [Theory]
     [InlineData("uma frase qualquer", 3)]
+    [InlineData( "O sol brilhava intensamente enquanto as crianças brincavam no parque", 10)]
+    [InlineData( "Um gato preto misterioso atravessou silenciosamente a rua deserta",9)]
     public void QuantityWord_ReceiveString_ReturnQuantityWord(string words, int quantityWord)
     {
         // act 
@@ -66,6 +81,10 @@ public class AnalyzerUnitTest
 
     [Theory]
     [InlineData("uma frase qualquer", 18)]
+    [InlineData( "O sol brilhava intensamente enquanto as crianças brincavam no parque", 68)]
+    [InlineData( "Um gato preto misterioso atravessou silenciosamente a rua deserta", 65)]
+    
+
   //  [InlineData()]
     public void QuantityChar_ReceiveString_ReturnQuantityChar(string words, int quantityChar)
     {
@@ -78,6 +97,8 @@ public class AnalyzerUnitTest
     }
     [Theory]
     [InlineData("uma frase qualquer", 6)]
+    [InlineData( "O sol brilhava intensamente enquanto as crianças brincavam no parque", 6)]
+    [InlineData( "Um gato preto misterioso atravessou silenciosamente a rua deserta", 7)]
     public void AverageChar_ReceiveString_ReturnAverageChar(string words, int average)
     {
         // act
